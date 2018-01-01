4b4fd6ca9db9b150682563501b5851075cf38d9c
joeyhage <joecool520@gmail.com>
2018-01-01 01:02:15 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # request-header-parser
+
 A request header parser microservice for the FreeCodeCamp Back End Development Certification.
+
+[https://www.freecodecamp.org/challenges/request-header-parser-microservice](https://www.freecodecamp.org/challenges/request-header-parser-microservice)
---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express'),
+      app = express();
+
+app.get('/*', (req, res) => {
+    res.status(200).json({
+        ipaddress: req.ip,
+	language: req.get('Accept-Language').split(',')[0],
+	software: req.get('User-Agent').match(/\(([^()]*)\)/)[1]
+    });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Express listening on port ${process.env.PORT || 3000}`);
+});
